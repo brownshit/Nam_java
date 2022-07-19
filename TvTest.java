27c577ba6ae49c2809e97ea0f3686f148286a47c
원종찬 <100653363+brownshit@users.noreply.github.com>
2022-07-19 16:22:23 +0900

    Update TvTest.java

--- TvTest.java
+++ TvTest.java
@@ -9,11 +9,15 @@ class Tv {
 }
 class TvTest{
     public static void main(String[] args){
-        Tv t;
-        t = new Tv();
-        t.channel = 7;
-        t.channelDown();
-        System.out.println("현재 채널은 "+t.channel+" 입니다.");
+        Tv t1 = new Tv(); Tv t2 = new Tv();
+        System.out.println("t1의 channel값은 "+t1.channel+" 입니다.");
+        System.out.println("t2의 channel값은 "+t2.channel+" 입니다.");
+
+        t1.channel= 7;
+        System.out.println("t1의 채널값을 "+t1.channel+"로 변경했습니다.");
+
+        System.out.println("t1의 channel값은 "+t1.channel+" 입니다.");
+        System.out.println("t2의 channel값은 "+t2.channel+" 입니다.");
+
     }
 }
-
